4393879bbb797be0ccc7407189f92ad5c54472d0
Arnaud Mesureur <arnaud.mesureur@gmail.com>
2018-03-09 01:06:31 +1100

    bundle new everlog

---
+++ lib/everlog.rb
@@ -0,0 +1,5 @@
+require "everlog/version"
+
+module Everlog
+  # Your code goes here...
+end
---
+++ spec/everlog_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Everlog do
+  it "has a version number" do
+    expect(Everlog::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
